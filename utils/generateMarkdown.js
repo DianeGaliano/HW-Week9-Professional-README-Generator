457d549707773cff9378cd09193069f828e7bf21
Diane Galiano <dianegaliano09@gmail.com>
2021-09-27 21:53:44 -0500

    pushing generateMarkdown changes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Appache") {
+    return
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,8 +15,40 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `
+  # <${response.title}>
+  ## Description
+  ${response.description}
+  
+  ## Table of Contents
+  - [Description](#usage)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${response.installation}
+  
+  ## Usage
+  ${response.usage}
+  
+  ## License
+  ${response.license}
+  
+  ## Contributing
+  ${response.colab}
+  
+  
+  ## Tests
+  ${response.tests}
+  
+  ## Questions
+  ${response.GitHub}
+  ${response.email}
 
 `;
 }
